faca989e6c67ae53e707b9a2c99b941670942371
cuong <12132077+cuongdcdev@users.noreply.github.com>
2024-09-01 05:33:10 +0700

    fix:add domain to nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -29,7 +29,11 @@ const nextConfig = {
       {
         protocol: "https",
         hostname:"assets.coingecko.com"
-      }
+      },
+      {
+        protocol: "https",
+        hostname: "indexer.nearcatalog.xyz",
+      },
     ],
   },
   async headers() {
